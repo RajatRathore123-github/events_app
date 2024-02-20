6dbfea2664e9e9193f5d43a4f770ace3ab157c85
Rajat Rathore <rajat12350iam@gmail.com>
2024-02-20 19:58:15 +0530

    style: Add home page in index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+
+  return (
+    <div>
+      <header>
+        <title>Events App</title>
+        <meta name="events app" content="Searching for events in your area? Look no further!"/>
+        <link rel="icon" href="/favicon.ico" />
+      </header>
+      <div></div>
+    </div>
+  );
+}
